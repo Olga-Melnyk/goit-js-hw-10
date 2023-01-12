a63c50fb4036515176f770ac090705305ed2b40e
Olga-Melnyk <3717129@ukr.net>
2023-01-12 15:41:27 +0200

    add queryselectors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('click', onSearch);
+
 const DEBOUNCE_DELAY = 300;
