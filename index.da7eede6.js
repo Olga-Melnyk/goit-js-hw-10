a06bff6f83fdc41fd77ac84e769a0ce44c159125
Olga-Melnyk <108472430+Olga-Melnyk@users.noreply.github.com>
2023-01-12 13:42:06 +0000

    Deploying to gh-pages from @ Olga-Melnyk/goit-js-hw-10@a63c50fb4036515176f770ac090705305ed2b40e 🚀

---
+++ index.da7eede6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-box");document.querySelector(".country-list"),document.querySelector(".country-info");e.addEventListener("click",onSearch)}();
+//# sourceMappingURL=index.da7eede6.js.map
