1ce882c809d358f31e0c863c7c626bc97a6b7def
Olga-Melnyk <3717129@ukr.net>
2023-01-13 20:34:53 +0200

    add index.js

--- src/index.js
+++ src/index.js
@@ -7,59 +7,68 @@ const input = document.querySelector('#search-box');
 const countrylist = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+let countryName = '';
 const DEBOUNCE_DELAY = 300;
 
 input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
-  const countryName = input.value;
+  countryName = input.value.trim();
   if (countryName === '') {
     countrylist.innerHTML = '';
     countryInfo.innerHTML = '';
     return;
-  }
-
-  fetchCountries(countryName)
-    .then(countries => {
-      if (countries.length > 10) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
+  } else {
+    fetchCountries(countryName)
+      .then(countries => {
+        if (countries.length < 10 && countries.length > 1) {
+          createList(countries);
+          countryInfo.innerHTML = '';
+        } else if (countries.length === 1) {
+          createCard(countries);
+          countrylist.innerHTML = '';
+        } else {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          countrylist.innerHTML = '';
+          countryInfo.innerHTML = '';
+        }
+      })
+      .catch(err => {
+        Notify.failure('Oops, there is no country with that name');
         countrylist.innerHTML = '';
         countryInfo.innerHTML = '';
-      }
-      if (countries.length <= 10) {
-        const markupList = countries.map(country => createList(country));
-        countrylist.innerHTML = markupList.json('');
-        countryInfo.innerHTML = '';
-      }
-      if (countries.length === 1) {
-        const markupInfo = countries.map(country => createCard(country));
-        countryInfo.innerHTML = markupInfo.json('');
-        countrylist.innerHTML = '';
-      }
-    })
-    .catch(err => {
-      Notify.failure('Oops, there is no country with that name');
-      countrylist.innerHTML = '';
-      countryInfo.innerHTML = '';
-      return err;
-    });
+        return err;
+      });
 
-  function createList({ flags, name }) {
-    return `<li>
-    <img src="${flags.svg}" alt="${name.official}">
-        <h2>${name.official}</h2>
-</li>`;
-  }
+    function createList(countries) {
+      const markupList = countries
+        .map(
+          country =>
+            `<li>
+    <img src="${country.flags.svg}" alt="${country.name.official}" width="40", height="30">
+        <p>${country.name.official}</p>
+</li>`
+        )
+        .join('');
+      countrylist.insertAdjacentHTML('beforeend', markupList);
+    }
 
-  function createCard({ name, capital, population, flags, languages }) {
-    return `<div>
-        <img src="${flags.svg}" alt="${name.official}" />
-        <h2>${name.official}</h2>
+    function createCard(countries) {
+      const country = countries[0];
+      const markupInfo = `<div><div>
+        <img src="${country.flags.svg}" alt="${
+        country.name.official
+      }" width="40", height="30" />
+        <h2>${country.name.official}</h2>
       </div>
-      <p><span>Capital:</span> ${capital}</p>
-      <p><span>Population:</span> ${population}</p>
-      <p><span>Languages:</span> ${languages}</p>`;
+      <p><span>Capital:</span> ${country.capital}</p>
+      <p><span>Population:</span> ${country.population}</p>
+      <p><span>Languages:</span> ${Object.values(country.languages).join(
+        ', '
+      )}</p></div>`;
+      countryInfo.innerHTML = markupInfo;
+    }
   }
 }
